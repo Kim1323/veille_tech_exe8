c93f31a7a6cedc5409847f3415435847add5a164
My Kim Duong <mykimduong13@gmail.com>
2017-02-21 17:47:56 -0500

    ajout du css du tableau

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 var http = require("http"); //importe le module serveur http
 var fs = require('fs');
 var obj;
-var objChaine;
 
 function afficherObjet(monObjet){
-	objChaine = "<table>";
+	obj = "<table>";
 	for (propriete in monObjet){
-		objChaine += "<tr>";
-		objChaine += "<th>" + propriete + "</th><td>" + monObjet[propriete] + "</td>";
-		objChaine += "</tr>";
+		obj += "<tr>";
+		obj += "<th>" + propriete + "</th><td>" + monObjet[propriete] + "</td>";
+		obj += "</tr>";
 	};
-	objChaine += "</table>";
+	obj += "</table>";
 };
 
 fs.readFile('07_fichier.json', 'utf8', function (err, data) {
   if (err) throw err;
-  obj = JSON.parse(data);
-  afficherObjet(obj);
+  afficherObjet(JSON.parse(data));
 });
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.write(objChaine);
+  response.write("<style>table,tr,th,td{border: black solid 1px}</style>");
+  response.write(obj);
   response.end();
 }).listen(3000);
