9a32871f6cc421de2979c80b7734ab9dba4c3342
My Kim Duong <mykimduong13@gmail.com>
2017-03-01 00:04:17 -0500

    fin de l exercice

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
-var http = require("http"); //importe le module serveur http
+var http = require('http');
 var fs = require('fs');
+var url = require('url');
 var obj;
 
+// Création d'un serveur
+http.createServer( function (request, response) {  
+   // Extraction de la requête «request» le chemin  qui nous donnera le nom de fichier
+   var pathname = url.parse(request.url).pathname;
+   
+  // Affichage du nom du fichier pour laquelle la requête a été généré
+   console.log("Request for " + pathname + " received.");
+
+   // Lire par le «fs» (file système) le fichier de la requête 
+   // le slice(1) permet de retirer le premier caractère
+   fs.readFile((pathname.slice(1) + ".json"), function (err, data) {
+    //Si la page n'est pas trouvée, affichage de l'erreur 404
+      if (err) {
+         console.log(err);
+         // HTTP Status: 404 : NOT FOUND
+         // Content Type: text/plain
+         response.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'});
+      //Si la page est trouvée, affichage d'un tableau
+      }else { 
+         //Page found   
+         // HTTP Status: 200 : OK
+         // Content Type: text/plain
+         response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+         response.write("<style>table,tr,th,td{border-collapse:collapse;border: black solid 1px;}</style>");
+          afficherObjet(JSON.parse(data));
+         // Affichage du contenu du fichier dans la page HTML
+          response.write(obj);
+      }
+      // transmet la reponse  
+      response.end();
+   });   
+}).listen(8081);
+
+//Mise des données du fichier JSON dans un tableau
 function afficherObjet(monObjet){
 	obj = "<table>";
 	for (propriete in monObjet){
@@ -11,15 +46,3 @@ function afficherObjet(monObjet){
 	};
 	obj += "</table>";
 };
-
-fs.readFile('07_fichier.json', 'utf8', function (err, data) {
-  if (err) throw err;
-  afficherObjet(JSON.parse(data));
-});
-
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<style>table,tr,th,td{border-collapse:collapse;border: black solid 1px;}</style>");
-  response.write(obj);
-  response.end();
-}).listen(3000);
